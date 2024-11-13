fffa77c097c8b99c4716aa9da67e1e8afcaed315
Uzih05 <luv.wlgjs@gmail.com>
2024-11-13 19:16:07 +0900

    login

---
+++ src/main/java/com/example/myweb/Service/EmailService.java
@@ -0,0 +1,27 @@
+package com.example.myweb.Service;
+
+import javax.mail.MessagingException;
+
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendEmail(String to, String subject, String text) throws MessagingException, jakarta.mail.MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, "utf-8");
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(text, true); // HTML 형식 허용
+        mailSender.send(message);
+    }
+}
